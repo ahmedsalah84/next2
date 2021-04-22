84c32386a0c273a4b2619992a3e3e2e1b4722a0e
Ahmed <asalah@spark-sys.com>
2021-04-22 14:22:54 +0200

    asa

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ export default function Home({ items }) {
             <div dangerouslySetInnerHTML={{ __html: items.content }} />
           </div>
         </div>
+
         
         <div className="col-lg-6">
           <img src={`${s.url + items.picture}?h=500`} className="img-fluid"  alt="" />
